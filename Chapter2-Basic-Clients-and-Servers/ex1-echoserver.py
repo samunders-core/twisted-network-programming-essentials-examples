33f85589995949138289df43896f3b02eaec8cdf
frstrtr <frstrtr@gmail.com>
2020-08-22 14:18:57 +0300

    echoserver-client remastered

--- Chapter2-Basic-Clients-and-Servers/ex1-echoserver.py
+++ Chapter2-Basic-Clients-and-Servers/ex1-echoserver.py
@@ -1,12 +1,44 @@
-from twisted.internet import protocol, reactor
+# ECHO SERVER
+
+from twisted.internet import protocol
+from twisted.internet.reactor import listenTCP, run
+import datetime
+
+counter = 0
+
 
 class Echo(protocol.Protocol):
+
+    # def makeConnection(self):
+    #     pass
+
+    def get_time_log(self):
+        t = datetime.datetime.now()
+        s = t.strftime('%H:%M:%S.%f')
+        return s
+
+    def connectionMade(self):
+        print('Connection established!!!\n')
+
+    def connectionLost(self, reason='Timeout'):
+        print(self.get_time_log(), 'Connection lost!!!\n')
+
     def dataReceived(self, data):
-        self.transport.write(data)
+        global counter
+        print(self.get_time_log(),
+              'Event: {} registered'.format(self.dataReceived))
+        print('Data::{}'.format(data))
+        print('Sending it back')
+        if self.transport.write(data) is None:
+            counter += 1
+            print(self.get_time_log(),
+                  'SUCCESS! {}\n\n'.format(counter))
+
 
 class EchoFactory(protocol.Factory):
     def buildProtocol(self, addr):
         return Echo()
 
-reactor.listenTCP(8000, EchoFactory())
-reactor.run()
+
+listenTCP(8000, EchoFactory())
+run()
