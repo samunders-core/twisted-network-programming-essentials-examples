65ce1827d32519f8847663bdc58fa78cb34b6bf5
frstrtr <frstrtr@gmail.com>
2020-08-24 11:30:58 +0300

    ex9 fix bytes and adds debug to console time

--- Chapter4-Web-Servers/ex9-blocking.py
+++ Chapter4-Web-Servers/ex9-blocking.py
@@ -4,11 +4,15 @@ from twisted.web.server import Site
 
 import time
 
+
 class BusyPage(Resource):
     isLeaf = True
+
     def render_GET(self, request):
+        print('start time: {}'.format(time.asctime()))
         time.sleep(5)
-        return "Finally done, at %s" % (time.asctime(),)
+        return ("Finally done, at %s" % (time.asctime(),)).encode()
+
 
 factory = Site(BusyPage())
 reactor.listenTCP(8000, factory)
