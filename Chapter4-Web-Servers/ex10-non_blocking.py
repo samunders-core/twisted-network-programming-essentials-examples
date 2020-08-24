0f1eb3c795346e7345509ec6440a7261971dc025
frstrtr <frstrtr@gmail.com>
2020-08-24 14:19:10 +0300

    ex10 fix bytes object

--- Chapter4-Web-Servers/ex10-non_blocking.py
+++ Chapter4-Web-Servers/ex10-non_blocking.py
@@ -5,17 +5,19 @@ from twisted.web.server import Site, NOT_DONE_YET
 
 import time
 
+
 class BusyPage(Resource):
     isLeaf = True
 
     def _delayedRender(self, request):
-        request.write("Finally done, at %s" % (time.asctime(),))
+        request.write(("Finally done, at %s" % (time.asctime(),)).encode())
         request.finish()
 
     def render_GET(self, request):
         d = deferLater(reactor, 5, lambda: request)
         d.addCallback(self._delayedRender)
         return NOT_DONE_YET
+
 
 factory = Site(BusyPage())
 reactor.listenTCP(8000, factory)
