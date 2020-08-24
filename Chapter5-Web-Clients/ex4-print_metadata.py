5e575f376669905e888b2e636a5e07e773a3a525
frstrtr <frstrtr@gmail.com>
2020-08-24 20:11:12 +0300

    ex4: fix bytes objects

--- Chapter5-Web-Clients/ex4-print_metadata.py
+++ Chapter5-Web-Clients/ex4-print_metadata.py
@@ -4,29 +4,35 @@ from twisted.internet import reactor
 from twisted.web.client import Agent
 from twisted.web.http_headers import Headers
 
+
 def printHeaders(response):
-    print 'HTTP version:', response.version
-    print 'Status code:', response.code
-    print 'Status phrase:', response.phrase
-    print 'Response headers:'
+
+    print('HTTP version:', *[x.decode() if type(x)
+                             is bytes else x for x in response.version])
+    print('Status code:', response.code)
+    print('Status phrase:', response.phrase.decode())
+    print('Response headers:')
     for header, value in response.headers.getAllRawHeaders():
-        print header, value
+        print(header.decode(), value[0].decode())  # .decode())
+
 
 def printError(failure):
-    print >>sys.stderr, failure
+    print(failure, file=sys.stderr)
+
 
 def stop(result):
     reactor.stop()
 
+
 if len(sys.argv) != 2:
-    print >>sys.stderr, "Usage: python print_metadata.py URL"
+    print("Usage: python print_metadata.py URL", file=sys.stderr)
     exit(1)
 
 agent = Agent(reactor)
 headers = Headers({'User-Agent': ['Twisted WebBot'],
                    'Content-Type': ['text/x-greeting']})
 
-d = agent.request('HEAD', sys.argv[1], headers=headers)
+d = agent.request(b'HEAD', sys.argv[1].encode(), headers=headers)
 d.addCallbacks(printHeaders, printError)
 d.addBoth(stop)
 
