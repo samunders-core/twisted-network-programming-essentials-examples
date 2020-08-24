2c540c5374a07520d6f275836ee26d14ab787f96
frstrtr <frstrtr@gmail.com>
2020-08-24 14:49:31 +0300

    fix: bytes object and method for agent.request note about uri format print py3 fix

--- Chapter5-Web-Clients/ex3-agent_print_resource.py
+++ Chapter5-Web-Clients/ex3-agent_print_resource.py
@@ -5,33 +5,40 @@ from twisted.internet.defer import Deferred
 from twisted.internet.protocol import Protocol
 from twisted.web.client import Agent
 
+
 class ResourcePrinter(Protocol):
     def __init__(self, finished):
         self.finished = finished
 
     def dataReceived(self, data):
-        print data
+        print(data)
 
     def connectionLost(self, reason):
         self.finished.callback(None)
+
 
 def printResource(response):
     finished = Deferred()
     response.deliverBody(ResourcePrinter(finished))
     return finished
 
+
 def printError(failure):
-    print >>sys.stderr, failure
+    print(failure, file=sys.stderr)
+
 
 def stop(result):
     reactor.stop()
 
+
 if len(sys.argv) != 2:
-    print >>sys.stderr, "Usage: python agent_print_resource.py URL"
+    print("Usage: python agent_print_resource.py URL", file=sys.stderr)
     exit(1)
 
 agent = Agent(reactor)
-d = agent.request('GET', sys.argv[1])
+# fix: bytes object and method
+d = agent.request(b'GET', uri=sys.argv[1].encode())
+# URI format: http://username:password@some_server:port
 d.addCallbacks(printResource, printError)
 d.addBoth(stop)
 
