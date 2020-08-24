ca98fc007572e95d548a5dd15e5bac9c53f4573e
frstrtr <frstrtr@gmail.com>
2020-08-24 14:33:54 +0300

    Incomplete fix for deprecated downloadPage

--- Chapter5-Web-Clients/ex2-download_resource.py
+++ Chapter5-Web-Clients/ex2-download_resource.py
@@ -1,18 +1,25 @@
 from twisted.internet import reactor
-from twisted.web.client import downloadPage
+# from twisted.web.client import downloadPage
+from twisted.web.client import Agent
 import sys
+
 
 def printError(failure):
     print >>sys.stderr, failure
 
+
 def stop(result):
     reactor.stop()
 
+
 if len(sys.argv) != 3:
-    print >>sys.stderr, "Usage: python download_resource.py <URL> <output file>"
+    # print >>sys.stderr, "Usage: python download_resource.py <URL> <output file>"
+    print("Usage: python download_resource.py <URL> <output file>", file=sys.stderr)
     exit(1)
 
-d = downloadPage(sys.argv[1], sys.argv[2])
+# d = downloadPage(sys.argv[1].encode(), sys.argv[2].encode()) # deprecated method
+# DeprecationWarning: twisted.web.client.downloadPage was deprecated in Twisted 16.7.0;
+# please use https://pypi.org/project/treq/ or twisted.web.client.Agent instead
 d.addErrback(printError)
 d.addBoth(stop)
 
